cd2ca2f1f93e0f465ba4bdeed801d607686e817b
Paul <paul@ruthorn.co.uk>
2015-10-25 23:37:37 +0000

    Add graphviz output

---
+++ aranea/visualisation.py
@@ -0,0 +1,22 @@
+from urllib.parse import urlparse
+from graphviz import Digraph
+
+
+def _process_page(g, page, include_resources=False):
+    s = lambda url: urlparse(url).path
+    for link_url in page.internal_urls - page.resource_urls:
+        g.edge(s(page.url), s(link_url))
+    if include_resources:
+        for link_url in page.resource_urls & page.internal_urls:
+            g.node(s(link_url), color='gray')
+            g.edge(s(page.url), s(link_url), color='gray')
+
+
+def make_graph(url, pages):
+    g = Digraph(comment='Site map generated from {!r}'.format(url))
+    for page_url, page in pages.items():
+        if isinstance(page, Exception):
+            g.node(page_url, label='{} {}'.format(page_url, page), color='red')
+        else:
+            _process_page(g, page)
+    return g
--- crawl.py
+++ crawl.py
@@ -4,6 +4,7 @@ import logging
 
 from aranea.limited_client import LimitedClientSession
 from aranea.crawler import crawl
+from aranea.visualisation import make_graph
 
 log = logging.getLogger(__name__)
 
@@ -15,10 +16,8 @@ if __name__ == '__main__':
         crawled_pages = loop.run_until_complete(crawl(
             client, 'http://aiohttp.readthedocs.org/en/stable/index.html',
             loop=loop))
-    except:
-        raise
-    else:
-        print(crawled_pages)
     finally:
         client.close()
         loop.close()
+    g = make_graph('url_here', crawled_pages)
+    print(g.source)
