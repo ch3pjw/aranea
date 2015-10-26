df9b53dac698252eb34edb2179e14749f7b99f95
Paul <paul@ruthorn.co.uk>
2015-10-26 00:24:05 +0000

    Dispatch crawler code from the CLI using argh...
    and Python 3 function annotations, which is jolly exciting!

--- aranea/visualisation.py
+++ aranea/visualisation.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse
 from graphviz import Digraph
 
 
-def _process_page(g, page, include_resources=False):
+def _process_page(g, page, include_resources):
     s = lambda url: urlparse(url).path
     for link_url in page.internal_urls - page.resource_urls:
         g.edge(s(page.url), s(link_url))
@@ -12,11 +12,11 @@ def _process_page(g, page, include_resources=False):
             g.edge(s(page.url), s(link_url), color='gray')
 
 
-def make_graph(url, pages):
+def make_graph(url, pages, include_resources=False):
     g = Digraph(comment='Site map generated from {!r}'.format(url))
     for page_url, page in pages.items():
         if isinstance(page, Exception):
             g.node(page_url, label='{} {}'.format(page_url, page), color='red')
         else:
-            _process_page(g, page)
+            _process_page(g, page, include_resources)
     return g
--- crawl.py
+++ crawl.py
@@ -1,6 +1,18 @@
 #! /usr/bin/env python
+'''Aranea is a toy web-crawler that outputs a graph (in 'dot' format) of
+the pages reachable by following links from the given url. It stays within
+the given domain.
+
+Direct the output to a file (e.g. example.dot) and convert using
+  dot -Tpdf example.dot > example.pdf
+or you can pipe the steps together to avoid generating the temporary file
+  aranea http://example.com/ | dot -Tpdf > example.pdf
+'''
+
 import asyncio
 import logging
+
+import argh
 
 from aranea.limited_client import LimitedClientSession
 from aranea.crawler import crawl
@@ -8,16 +20,32 @@ from aranea.visualisation import make_graph
 
 log = logging.getLogger(__name__)
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+
+def main(url, max_concurrent_requests, resources):
     loop = asyncio.get_event_loop()
-    client = LimitedClientSession(3, loop=loop)
+    client = LimitedClientSession(max_concurrent_requests, loop=loop)
     try:
-        crawled_pages = loop.run_until_complete(crawl(
-            client, 'http://aiohttp.readthedocs.org/en/stable/index.html',
-            loop=loop))
+        crawled_pages = loop.run_until_complete(
+            crawl(client, url, loop=loop))
     finally:
         client.close()
         loop.close()
-    g = make_graph('url_here', crawled_pages)
+    return make_graph('url_here', crawled_pages, resources)
+
+
+def cli(
+        url: 'The url from which to start crawling',
+        max_concurrent_requests:
+            'The maximum number of concurrent HTTP requests to handle during '
+            'crawling' = 3,
+        resources:
+            'Whether to output page resources (e.g. script files) in the '
+            'output' = False):
+    g = main(url, max_concurrent_requests, resources)
     print(g.source)
+cli.__doc__ = __doc__
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    argh.dispatch_command(cli)
